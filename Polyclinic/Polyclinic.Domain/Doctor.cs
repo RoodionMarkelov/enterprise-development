23ebff3215087018224af51a314decc79b30099e
user <rodionmarkelov01229@gmail.com>
2025-09-24 22:04:34 +0400

    поправочки

--- Polyclinic/Polyclinic.Domain/Doctor.cs
+++ Polyclinic/Polyclinic.Domain/Doctor.cs
@@ -1,50 +1,49 @@
-﻿namespace Polyclinic.Domain
+﻿namespace Polyclinic.Domain;
+
+/// <summary>
+/// Перечисление специализаций врачей
+/// </summary>
+public enum Specialization
+{
+    Therapist,
+    Surgeon,
+    Cardiologist,
+    Neurologist,
+    Pediatrician,
+    Dentist,
+    Ophthalmologist,
+    Dermatologist,
+    Orthopedist,
+}
+
+/// <summary>
+/// Класс для описания доктора
+/// </summary>
+public class Doctor
 {
     /// <summary>
-    /// Перечисление специализаций врачей
+    /// Поле характеризующее номер паспорта доктора
     /// </summary>
-    public enum Specialization
-    {
-        Therapist,
-        Surgeon,
-        Cardiologist,
-        Neurologist,
-        Pediatrician,
-        Dentist,
-        Ophthalmologist,
-        Dermatologist,
-        Orthopedist,
-    }
+    public required string IdPassport { get; set; }
 
     /// <summary>
-    /// Класс для описания доктора
+    ///  Поле характеризующее имя доктора
     /// </summary>
-    public class Doctor
-    {
-        /// <summary>
-        /// Поле характеризующее номер паспорта доктора
-        /// </summary>
-        public required string IdPassport { get; set; }
+    public required string Name { get; set; }
 
-        /// <summary>
-        ///  Поле характеризующее имя доктора
-        /// </summary>
-        public required string Name { get; set; }
+    /// <summary>
+    /// Поле характерезующее дату дня рождения доктора
+    /// </summary>
+    public DateTime Birthday { get; init; }
 
-        /// <summary>
-        /// Поле характерезующее дату дня рождения доктора
-        /// </summary>
-        public DateTime Birthday { get; init; }
+    /// <summary>
+    /// Поле характерезующее специализацию доктора
+    /// </summary>
+    public Specialization SpecializationOfDoctor { get; set; }
 
-        /// <summary>
-        /// Поле характерезующее специализацию доктора
-        /// </summary>
-        public Specialization SpecializationOfDoctor { get; set; }
+    /// <summary>
+    /// Поле характерезующее стаж доктора
+    /// </summary>
+    public int WorkExperience { get; set; }
 
-        /// <summary>
-        /// Поле характерезующее стаж доктора
-        /// </summary>
-        public int WorkExperience { get; set; }
-
-    }
 }
