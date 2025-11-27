04aadff7fb7e481a5231b594665a76b002d1bb0c
user <rodionmarkelov01229@gmail.com>
2025-11-27 14:17:14 +0400

    удалил переменную myService

--- Polyclinic/AppHost/AppHost.cs
+++ Polyclinic/AppHost/AppHost.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var db = builder.AddSqlServer("sql").AddDatabase("db");
 
-var myService = builder.AddProject<Projects.Api>("api") // без неё в докере не поднимется swagger
+builder.AddProject<Projects.Api>("api")
                        .WithReference(db)
                        .WaitFor(db);
 
